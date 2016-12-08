642da0bf4a888bb582481afbe0ca1e256af192ff
Phil Daly <PDaly@Lsst.org>
2016-12-08 13:53:41 -0700

    Added xterm titles

--- bin/run.event.loggers.sh
+++ bin/run.event.loggers.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-xterm -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsEntityStartup.py &
-
-xterm -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsEntityShutdown.py &
-
-xterm -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsCommandIssued.py &
-
-xterm -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsCommandStatus.py &
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T logevent_ocsEntityStartup  -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsEntityStartup.py &
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T logevent_ocsEntityShutdown -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsEntityShutdown.py &
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T logevent_ocsCommandIssued  -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsCommandIssued.py &
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T logevent_ocsCommandStatus  -e python $TS_OCS_SEQUENCER_BIN/logevent_ocsCommandStatus.py &
